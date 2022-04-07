fca500be9d79deeb4320c440769a2d42e7c56eaa
Stavros_Kanias <77228576+StavrosKanias@users.noreply.github.com>
2022-04-07 15:22:00 +0300

    format

--- solution.py
+++ solution.py
@@ -9,19 +9,15 @@ def main():
     osSlash = demand[0][6]
     ourDf = pandas.DataFrame()
     for file in demand:
-<<<<<<< HEAD
-        year, month, day = file.split(
-            '/')[1][:4], file.split('/')[1][4:6], file.split('/')[1][6:8]
-=======
-        year , month , day = file.split(osSlash)[1][:4] , file.split(osSlash)[1][4:6] , file.split(osSlash)[1][6:8]
->>>>>>> 27ffa0ea27a80c67ec2f4b827a32f18ed2c6edc8
+        year, month, day = file.split(osSlash)[1][:4], file.split(osSlash)[
+            1][4:6], file.split(osSlash)[1][6:8]
         # print (f'{year=} {month=} {day=}')
         try:
             datetime.strptime(month + '/' + day + '/' + year, '%m/%d/%Y')
         except ValueError:
             print(file)
             continue
-        name = file.split('/')[1]
+        name = file.split(osSlash)[1]
         # print(file)
         df = pandas.read_csv(file)
     print(df)
