9148c19bc14f65d44fd0cb8c8b55cdaa00911144
KonstantoJr <konstantokon29@gmail.com>
2022-04-07 15:52:25 +0300

    added day mean calculation

--- solution.py
+++ solution.py
@@ -1,26 +1,46 @@
 import pandas
 import glob
 from datetime import datetime
+import math
 
 
 def main():
     demand = glob.glob("demand/*.csv")
     demand.sort()
     osSlash = demand[0][6]
-    ourDf = pandas.DataFrame()
+    days = []
+    means = []
     for file in demand:
         year, month, day = file.split(osSlash)[1][:4], file.split(osSlash)[
             1][4:6], file.split(osSlash)[1][6:8]
-        # print (f'{year=} {month=} {day=}')
+        # Check if day exists
         try:
-            datetime.strptime(month + '/' + day + '/' + year, '%m/%d/%Y')
+            date = datetime.strptime(
+                month + '/' + day + '/' + year, '%m/%d/%Y')
         except ValueError:
-            print(file)
             continue
+
         name = file.split(osSlash)[1]
-        # print(file)
+        # Read csv
         df = pandas.read_csv(file)
-    print(df)
+        mean = dayMeanValue(df)
+        days.append(date)
+        means.append(mean)
+    ourDf = pandas.DataFrame({"Day": days, "Mean": means})
+    print(ourDf)
+
+
+def dayMeanValue(df):
+    keys = list(df.columns.values)
+    base = len(df[keys[-1]])
+    total = 0
+    cnt = 0
+    for value in df[keys[-1]]:
+        if math.isnan(value):
+            cnt += 1
+            continue
+        total += value
+    return total / (base - cnt)
 
 
 if __name__ == "__main__":
