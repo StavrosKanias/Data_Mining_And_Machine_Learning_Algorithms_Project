e8c482bb0e644985fc8da1847b3aa779f668848e
KonstantoJr <konstantokon29@gmail.com>
2022-04-14 15:56:59 +0300

    added day ahead forecast at plot

--- solution.py
+++ solution.py
@@ -13,6 +13,7 @@ def main():
     osSlash = demands[0][6]
     days = []
     means = []
+    expectedMeans = []
     for demand, source in zip(demands, sources):
         year, month, day = demand.split(osSlash)[1][:4], demand.split(osSlash)[
             1][4:6], demand.split(osSlash)[1][6:8]
@@ -25,44 +26,58 @@ def main():
 
         # Read csv
         dfDemand = pandas.read_csv(demand)
-        dfSource = pandas.read_csv(source)
-        mean = dayMeanDemand(dfDemand)
+        # dfSource = pandas.read_csv(source)
+        mean = dayMeanDemand(dfDemand, "Current demand")
+        expectedMean = dayMeanDemand(dfDemand, "Day ahead forecast")
         days.append(date)
         means.append(mean)
-    ourDf = pandas.DataFrame({"Day": days, "Mean": means})
+        expectedMeans.append(expectedMean)
+    demandDf = pandas.DataFrame({"Day": days, "Mean": means})
+    expectedDemandDf = pandas.DataFrame(
+        {"Day": days, "Mean": expectedMeans})
 
-    plotdf(ourDf, "Monthly demand")
+    plotdf(demandDf, expectedDemandDf, "Monthly demand", 0)
+    plt.show()
 
 
-def plotdf(df, title):
+def plotdf(demandDf, expectedDemandDf, title, figure):
     days = []
     means = []
     total = []
+    expectedDemand = []
     prevyear = '2019'
-    plt.figure(0)
+    plt.figure(figure)
     cnt = 0
-    for day, mean in zip(df["Day"], df["Mean"]):
+    for day, mean, expected in zip(demandDf["Day"], demandDf["Mean"], expectedDemandDf["Mean"]):
         year = str(day)[:4]
         if prevyear == year:
             days.append(day)
             means.append(mean)
+            expectedDemand.append(expected)
         else:
             cnt += 1
             prevyear = year
             plt.subplot(3, 1, cnt)
             plt.title(f"{title} for the year {(int(prevyear) - 1)}")
-            plt.plot_date(days, means, "b-", xdate=True)
-            plt.plot_date(days, means, "r.", xdate=True)
+            # plt.plot_date(days, means, "b-", xdate=True)
+            plt.plot_date(days, means, "rx", xdate=True)
+            ###
+            temp = expectedDemand.pop(-1)
+            ###
+            plt.plot_date(days[1 if cnt == 1 else 0:],
+                          expectedDemand, "b.", xdate=True)
+
             total.append((list(days), list(means)))
             days.clear()
             means.clear()
+            expectedDemand.clear()
+            expectedDemand.extend((temp, expected))
             days.append(day)
             means.append(mean)
     plt.subplot(3, 1, 3)
     plt.title(f"{title} for the year {(int(prevyear))}")
-    plt.plot_date(days, means, "b-", xdate=True)
-    plt.plot_date(days, means, "r.", xdate=True)
-    plt.show()
+    plt.plot_date(days, expectedDemand[:-1], "rx", xdate=True)
+    plt.plot_date(days, means, "b.", xdate=True)
 
 
 def dayMeanSource(df):
@@ -70,17 +85,18 @@ def dayMeanSource(df):
     base = len(df[keys[-1]])
 
 
-def dayMeanDemand(df):
+def dayMeanDemand(df, column):
     keys = list(df.columns.values)
-    base = len(df[keys[-1]])
-    total = 0
-    cnt = 0
-    for value in df[keys[-1]]:
-        if math.isnan(value):
-            cnt += 1
-            continue
-        total += value
-    return total / (base - cnt)
+    if column in keys:
+        base = len(df[column])
+        total = 0
+        cnt = 0
+        for value in df[column]:
+            if math.isnan(value):
+                cnt += 1
+                continue
+            total += value
+        return total / (base - cnt)
 
 
 if __name__ == "__main__":
