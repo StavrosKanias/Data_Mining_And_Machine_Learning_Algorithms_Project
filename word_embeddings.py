eb3b6a8707ae38c5120b2de24ddb666609adddd9
KonstantoJr <konstantokon29@gmail.com>
2022-06-14 12:26:42 +0300

    probably done with embeddings

--- word_embeddings.py
+++ word_embeddings.py
@@ -116,9 +116,9 @@ if __name__ == "__main__":
 
     model_name = "MyWord2Vec.model"
     # Set values for various word2vec parameters
-    num_features = 300    # Word vector dimensionality
+    num_features = 400    # Word vector dimensionality
     min_word_count = 40   # Minimum word count
-    num_workers = 3       # Number of threads to run in parallel
+    num_workers = 4      # Number of threads to run in parallel
     context = 10          # Context window size
     downsampling = 1e-3   # Downsample setting for frequent words
     if not os.path.exists(model_name):
@@ -129,7 +129,7 @@ if __name__ == "__main__":
 
         # If you don't plan to train the model any further, calling
         # init_sims will make the model much more memory-efficient.
-        model.init_sims(replace=True)
+        # model.init_sims(replace=True)
 
         # It can be helpful to create a meaningful model name and
         # save the model for later use. You can load it later using Word2Vec.load()
