430dad6b0bc9383943f833a6fc2d2b95bc7de778
KonstantoJr <konstantokon29@gmail.com>
2022-06-14 18:52:49 +0300

    made neuron model

--- demand_prediction.py
+++ demand_prediction.py
@@ -1,55 +1,89 @@
-from sklearn.cluster import KMeans
-from sklearn.manifold import TSNE
-from sklearn.preprocessing import MinMaxScaler
-from sklearn.metrics import silhouette_score
-from sklearn.cluster import DBSCAN
-from sklearn.neighbors import NearestNeighbors
-from collections import Counter
 import seaborn as sns
 import numpy as np
-from sklearn import metrics
 import pandas as pd
 import matplotlib.pyplot as plt
-import matplotlib.colors
+import math
+from keras.models import Sequential
+from keras.layers import Dense
+from keras.layers import LSTM
+from sklearn.preprocessing import MinMaxScaler
+from sklearn.metrics import mean_squared_error
+np.random.seed(7)
 
 
-def create_dataset():
-    df = pd.read_csv("unified.csv")
-    df['Datetime'] = pd.to_datetime(df['Datetime'])
-    df = df.set_index('Datetime')
-    df = df.astype(np.float64).fillna(method='bfill')
-
-    # print(df)
-
-    # For simplication,
-    # I will resample so that each row
-    # represents a whole hour
-    df_uci_daily = df.resample('d').sum()
-    df_uci_daily['day'] = df_uci_daily.index.day
-    df_uci_daily.index = df_uci_daily.index.date
-    df_uci_daily.drop(['day'], axis=1, inplace=True)
-    return df_uci_daily
+# convert an array of values into a dataset matrix
+def create_dataset(dataset, look_back=1):
+    dataX, dataY = [], []
+    for i in range(len(dataset)-look_back-1):
+        a = dataset[i:(i+look_back), 0]
+        dataX.append(a)
+        dataY.append(dataset[i + look_back, 0])
+    return np.array(dataX), np.array(dataY)
 
 
-def series_to_supervised(data, n_in=1, n_out=1, dropnan=True):
-    n_vars = 1 if type(data) is list else data.shape[1]
-    dff = pd.DataFrame(data)
-    cols, names = list(), list()
-    for i in range(n_in, 0, -1):
-        cols.append(dff.shift(-i))
-        names += [('var%d(t-%d)' % (j+1, i)) for j in range(n_vars)]
-    for i in range(0, n_out):
-        cols.append(dff.shift(-i))
-        if i == 0:
-            names += [('var%d(t)' % (j+1)) for j in range(n_vars)]
-        else:
-            names += [('var%d(t+%d)' % (j+1)) for j in range(n_vars)]
-        agg = pd.concat(cols, axis=1)
-        agg.columns = names
-        if dropnan:
-            agg.dropna(inplace=True)
-        return agg
+def read_dataset():
+    df_demand = pd.read_csv("unified.csv")
+    df_sources = pd.read_csv("summedSources.csv")
+    df_demand.drop('Supply', axis=1, inplace=True)
+    df_demand.drop('Datetime', axis=1, inplace=True)
+    df = df_sources.join(df_demand)
+    df['Demand-Renewable'] = df['Demand'] - df['Renewable']
+    df = df.iloc[:110000, :]
+    df = df.dropna()
+    return df
 
 
 if __name__ == "__main__":
-    df = create_dataset()
+    df = read_dataset()
+    print(df)
+    cols = ['Demand-Renewable']
+    dataframe = df[cols]
+    dataset = dataframe.values
+    dataset = dataset.astype('float32')
+    # normalize the dataset
+    scaler = MinMaxScaler(feature_range=(0, 1))
+    dataset = scaler.fit_transform(dataset)
+    # split into train and test sets
+    train_size = int(len(dataset) * 0.67)
+    test_size = len(dataset) - train_size
+    train, test = dataset[0:train_size, :], dataset[train_size:len(dataset), :]
+    # print(len(train), len(test))
+    trainX, trainY = create_dataset(train, look_back=1)
+    testX, testY = create_dataset(test, look_back=1)
+    # reshape input to be [samples, time steps, features]
+    trainX = np.reshape(trainX, (trainX.shape[0], 1, trainX.shape[1]))
+    testX = np.reshape(testX, (testX.shape[0], 1, testX.shape[1]))
+    # create and fit the LSTM network
+    model = Sequential()
+    model.add(LSTM(16, input_shape=(1, 1)))
+    model.add(Dense(1))
+    model.compile(loss='mean_squared_error', optimizer='adam')
+    model.fit(trainX, trainY, epochs=5, batch_size=64)
+
+    # make predictions
+    trainPredict = model.predict(trainX)
+    testPredict = model.predict(testX)
+    # invert predictions
+    trainPredict = scaler.inverse_transform(trainPredict)
+    trainY = scaler.inverse_transform([trainY])
+    testPredict = scaler.inverse_transform(testPredict)
+    testY = scaler.inverse_transform([testY])
+    # calculate root mean squared error
+    trainScore = math.sqrt(mean_squared_error(trainY[0], trainPredict[:, 0]))
+    print('Train Score: %.2f RMSE' % (trainScore))
+    testScore = math.sqrt(mean_squared_error(testY[0], testPredict[:, 0]))
+    print('Test Score: %.2f RMSE' % (testScore))
+
+    # shift train predictions for plotting
+    trainPredictPlot = np.empty_like(dataset)
+    trainPredictPlot[:, :] = np.nan
+    trainPredictPlot[1:len(trainPredict)+1, :] = trainPredict
+    # shift test predictions for plotting
+    testPredictPlot = np.empty_like(dataset)
+    testPredictPlot[:, :] = np.nan
+    testPredictPlot[len(trainPredict)+(1*2)+1:len(dataset)-1, :] = testPredict
+    # plot baseline and predictions
+    plt.plot(scaler.inverse_transform(dataset))
+    plt.plot(trainPredictPlot)
+    plt.plot(testPredictPlot)
+    plt.show()
